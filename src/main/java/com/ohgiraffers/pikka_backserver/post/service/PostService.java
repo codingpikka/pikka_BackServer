387b2809771339c3186adaccc777df37705654b0
wsy <wsy9088@gmail.com>
2024-08-08 15:20:39 +0900

    feat post, questions

--- src/main/java/com/ohgiraffers/pikka_backserver/post/service/PostService.java
+++ src/main/java/com/ohgiraffers/pikka_backserver/post/service/PostService.java
@@ -7,8 +7,10 @@ import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Service
 public class PostService {
@@ -25,8 +27,8 @@ public class PostService {
         PostEntity postEntity = new PostEntity();
         postEntity.setId(postDTO.getId());
         postEntity.setTitle(postDTO.getTitle());
-        postEntity.setContent(postDTO.getContent());
         postEntity.setThumbnail(postDTO.getThumbnail());
+        postEntity.setContent(postDTO.getContent());
 
         PostEntity savedPostEntity = postRepository.save(postEntity);
 
@@ -36,5 +38,19 @@ public class PostService {
         }else {
             return Map.of("postId", savedPostEntity.getId());
         }
+    }
+
+    public List<PostDTO> getAllPosts() {
+        List<PostEntity> posts = postRepository.findAll();
+        return posts.stream()
+                .map(post -> {
+                    PostDTO dto = new PostDTO();
+                    dto.setId(post.getId());
+                    dto.setTitle(post.getTitle());
+                    dto.setThumbnail(post.getThumbnail());
+                    dto.setContent(post.getContent());
+                    return dto;
+                })
+                .collect(Collectors.toList());
     }
 }
