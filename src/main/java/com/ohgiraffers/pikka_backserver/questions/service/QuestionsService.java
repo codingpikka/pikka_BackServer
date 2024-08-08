aa5b17b1fb6b5f98d8dd118188897150c6b2d805
Moka <yisida60@gmail.com>
2024-08-08 11:40:22 +0900

    update controller,dto,entity,service

--- src/main/java/com/ohgiraffers/pikka_backserver/questions/service/QuestionsService.java
+++ src/main/java/com/ohgiraffers/pikka_backserver/questions/service/QuestionsService.java
@@ -15,13 +15,16 @@ public class QuestionsService {
     @Autowired
     private QuestionsRepository questionsRepository;
 
-    //    public List<QuestionsDTO> getAllQuestions(){
-//        return questionsRepository.findAll().stream()
-//                .map(this::convertToDTO)
-//                .collect(Collectors.toList());
-//    }
+    // 기존 메서드 유지
     public List<QuestionsEntity> getAllQuestions() {
         return questionsRepository.findAll();
+    }
+
+    // 새로운 메서드 추가
+    public List<QuestionsDTO> getAllQuestionsDTO() {
+        return questionsRepository.findAll().stream()
+                .map(this::convertToDTO)
+                .collect(Collectors.toList());
     }
 
     public QuestionsDTO getQuestionById(Long contactId){
@@ -29,15 +32,13 @@ public class QuestionsService {
         return convertToDTO(questionsEntity);
     }
 
-    // 새로운 질문을 저장하는 메서드 추가
     public QuestionsEntity addQuestion(QuestionsDTO questionsDTO) {
-//        QuestionsEntity questionsEntity = convertToEntity(questionsDTO);
         QuestionsEntity questionsEntity = new QuestionsEntity();
-
         questionsEntity.setContactId(questionsDTO.getContactId());
         questionsEntity.setUserId(questionsDTO.getUserId());
         questionsEntity.setUserName(questionsDTO.getUserName());
         questionsEntity.setContactType(questionsDTO.getContactType());
+        questionsEntity.setContactTitle(questionsDTO.getContactTitle()); // 제목 설정
         questionsEntity.setContactContents(questionsDTO.getContactContents());
         questionsEntity.setContactPostedDate(questionsDTO.getContactPostedDate());
         questionsEntity.setAdminId(questionsDTO.getAdminId());
@@ -47,8 +48,6 @@ public class QuestionsService {
         questionsEntity.setResponsePostedDate(questionsDTO.getResponsePostedDate());
         questionsEntity.setResponseStatus(questionsDTO.getResponseStatus());
         questionsEntity.setAnswerContent(questionsDTO.getAnswerContent());
-        QuestionsEntity savedEntity = questionsRepository.save(questionsEntity);
-//        return convertToDTO(savedEntity);
         return questionsRepository.save(questionsEntity);
     }
 
@@ -77,6 +76,7 @@ public class QuestionsService {
         questionsDTO.setUserId(questionsEntity.getUserId());
         questionsDTO.setUserName(questionsEntity.getUserName());
         questionsDTO.setContactType(questionsEntity.getContactType());
+        questionsDTO.setContactTitle(questionsEntity.getContactTitle()); // 제목 설정
         questionsDTO.setContactContents(questionsEntity.getContactContents());
         questionsDTO.setContactPostedDate(questionsEntity.getContactPostedDate());
         questionsDTO.setAdminId(questionsEntity.getAdminId());
@@ -95,6 +95,7 @@ public class QuestionsService {
         questionsEntity.setUserId(questionsDTO.getUserId());
         questionsEntity.setUserName(questionsDTO.getUserName());
         questionsEntity.setContactType(questionsDTO.getContactType());
+        questionsEntity.setContactTitle(questionsDTO.getContactTitle()); // 제목 설정
         questionsEntity.setContactContents(questionsDTO.getContactContents());
         questionsEntity.setContactPostedDate(questionsDTO.getContactPostedDate());
         questionsEntity.setAdminId(questionsDTO.getAdminId());
