632a6f70a98ded6f9a63ec7aceb51f449a904d79
wsy <wsy9088@gmail.com>
2024-08-08 18:45:10 +0900

    feat post

--- src/main/java/com/ohgiraffers/pikka_backserver/post/controller/PostController.java
+++ src/main/java/com/ohgiraffers/pikka_backserver/post/controller/PostController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+//확인
 @RestController
 @RequestMapping("/api")
 public class PostController {
